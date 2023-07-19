916aef4a8c725acd81f246fd3f2ea35642a83f3d
D-Jy <duan@d-jy.net>
2023-07-19 23:41:23 +0800

    Update DepthCamera.py

--- DepthCamera.py
+++ DepthCamera.py
@@ -1,11 +1,3 @@
-'''
-Author: D-Jy
-Date: 2023-07-12 09:42:36
-LastEditTime: 2023-07-19 15:03:44
-LastEditors: D-Jy
-Description: DepthCamera
-FilePath: .\DepthCamera.py
-'''
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -13,32 +5,32 @@ import cv2
 class RGBD:
     def __init__(self, color_image, depth_image):
         """
-        初始化RGBD对象
+        Initialize the RGBD object with color and depth image data.
         """
         self.color_image = color_image
         self.depth_image = depth_image
 
     def getImage(self):
         """
-        返回彩色图像numpy数组
+        Return the stored color image as a numpy array.
         """
         return self.color_image
 
     def getDepth(self):
         """
-        返回深度信息numpy数组
+        Return the stored depth data as a numpy array.
         """
         return self.depth_image
 
     def saveImage(self, path):
         """
-        将彩色图像保存成图片文件
+        Save the stored color image to a file at the given path.
         """
         cv2.imwrite(path, self.color_image)
 
     def getDepthPoint(self, height, width):
         """
-        获取当前深度信息（摄像头到某个像素点位置的距离）
+        Get the depth data for a specific point in the depth image.
         """
         return self.depth_image[height, width]
 
@@ -46,59 +38,59 @@ class RGBD:
 class DepthCamera:
     def __init__(self):
         """
-        初始化深度摄像头
+        Initialize the depth camera and start the camera pipeline.
         """
         self.pipeline = rs.pipeline()
         self.config = rs.config()
         self.pipeline.start(self.config)
 
-    def getImage(self):
+    def _get_frames(self):
         """
-        获取当前彩色图像并返回一个numpy数组
+        Capture frames from the camera and return color and depth frames.
         """
         frames = self.pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
-        color_image = np.asanyarray(color_frame.get_data())
-        return color_image
+        depth_frame = frames.get_depth_frame()
+        return color_frame, depth_frame
+
+    def getImage(self):
+        """
+        Capture a color image from the camera and return it as a numpy array.
+        """
+        color_frame, _ = self._get_frames()
+        return np.asanyarray(color_frame.get_data())
 
     def saveImage(self, path):
         """
-        获取当前彩色图像并根据传入路径保存图片文件
+        Capture a color image from the camera and save it to a file.
         """
-        color_image = self.getImage()
-        cv2.imwrite(path, color_image)
+        cv2.imwrite(path, self.getImage())
 
     def getDepthPoint(self, height, width):
         """
-        获取当前深度信息（摄像头到某个像素点位置的距离）
+        Get the depth data for a specific point in the depth image.
         """
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        distance = depth_frame.get_distance(width, height)
-        return distance
+        _, depth_frame = self._get_frames()
+        return depth_frame.get_distance(width, height)
 
     def getDepth(self):
         """
-        获取当前深度信息，直接返回一个numpy数组
+        Capture depth data from the camera and return it as a numpy array.
         """
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        depth_image = np.asanyarray(depth_frame.get_data())
-        return depth_image
+        _, depth_frame = self._get_frames()
+        return np.asanyarray(depth_frame.get_data())
 
     def getRGBD(self):
         """
-        获取当前彩色图像和图像深度信息
+        Capture color and depth data from the camera and return as an RGBD object.
         """
-        frames = self.pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        depth_frame = frames.get_depth_frame()
+        color_frame, depth_frame = self._get_frames()
         color_image = np.asanyarray(color_frame.get_data())
         depth_image = np.asanyarray(depth_frame.get_data())
         return RGBD(color_image, depth_image)
 
     def close(self):
         """
-        关闭深度摄像头
+        Stop the camera pipeline.
         """
         self.pipeline.stop()
